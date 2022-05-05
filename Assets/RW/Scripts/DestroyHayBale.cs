18ee62ed27c5923409d1d1b91fc5f3d8ecad5672
MarioElProgramador <mario.valenzuela01@estudiant.upf.edu>
2022-05-05 22:44:41 +0200

    Initial commit

---
+++ Assets/RW/Scripts/DestroyHayBale.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHayBale : MonoBehaviour
+{
+    public string tagFilter;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag(tagFilter)) 
+        {
+            Destroy(other.gameObject); 
+        }
+    }
+}
---
+++ Assets/RW/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed;
+
+    float horizontalBoundary = 22;
+
+    Vector2 HorizontalVerticalInput;
+
+    public GameObject hayBalePrefab; //Reference to the Hay Bale prefab.
+    public Transform haySpawnpoint; //The point from which the hay will to be shot.
+    public float shootInterval; //The smallest amount of time between shots
+    private float shootTimer; //A timer that to keep track whether the machine can shoot
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    private void ShootHay()
+    {
+        hayBalePrefab.tag = "Hay";
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+
+    private void UpdateMovement()
+    {
+        HorizontalVerticalInput[0] = Input.GetAxisRaw("Horizontal");
+        HorizontalVerticalInput[1] = Input.GetAxisRaw("Vertical");
+
+        // Left, Up
+        if ((HorizontalVerticalInput[0] < 0 || HorizontalVerticalInput[1] > 0) && transform.position.x > -horizontalBoundary) // Left Boundary
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        // Right, Down
+        else if ((HorizontalVerticalInput[0] > 0 || HorizontalVerticalInput[1] < 0) && transform.position.x < horizontalBoundary) // Right Boundary
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+}
